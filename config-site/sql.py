13b8682b39b7d9192b6263719c38750e39314176
Zame76 <sampo.juntunen@gmail.com>
2023-11-24 12:14:11 +0200

    modified file structure, added database files, modified databases

--- config-site/sql.py
+++ config-site/sql.py
@@ -1,12 +1,14 @@
 import sqlite3
+import config
 
-path = "static/db/website.db"
+# Set the path to database file
+path = config.database_path + "website.db"
 
 # Create tables to database if they don't exist already
 def createTables():
     conn = sqlite3.connect(path)
     sql = conn.cursor()
-    # Create Weather table
+    # Create the Weather table
     clause = "create table if not exists Weather ("                 
     clause += "WID integer primary key autoincrement, "     
     clause += "LoggedTime datetime not null, "
@@ -20,13 +22,29 @@ def createTables():
     clause += "RainDescEn text)"
     sql.execute(clause)
 
-    # Create Photo table
+    # Create the Photo table
     clause = "create table if not exists Photo ("
     clause += "PID integer primary key autoincrement, "
     clause += "LoggedTIme datetime not null, "
     clause += "MeasuredTime text, "
     clause += "B64Photo text)"
+    sql.execute(clause)    
+
+    # Create the ElectricityLog table
+    clause = "create table if not exists ElectricityLog ("
+    clause += "DataFetched date)"
     sql.execute(clause)
+
+    # Create the ElectricityPrices table
+    clause = "create table if not exists ElectricityPrices ("
+    clause += "EID integer primary key autoincrement, "
+    clause += "PriceDate date, "
+    clause += "TimeID integert, "
+    clause += "DateHour integer, "
+    clause += "Price real)"
+    sql.execute(clause)
+
+    # Commit the changes to the database
     conn.commit()
     conn.close()
 
@@ -34,10 +52,16 @@ def createTables():
 def insertWeather(parameters):
     conn = sqlite3.connect(path)
     sql = conn.cursor()
+    # Clean up Weather table from old data, here we need to trim LoggedTime to Y-m-s format and delete everything that is not 
+    # from today.
+    clause = "delete from Weather where date(LoggedTime) < date()"
+    sql.execute(clause)
+    # Insert the data from parameters into the Weather table
     clause = "insert into Weather "
     clause += "(LoggedTime, MeasuredTime, Temperature, WindSpeed, WindDirection, Moisture, Rain, RainDescFi, RainDescEn) "
     clause += "values (?, ?, ?, ?, ?, ?, ?, ?, ?)"
     sql.execute(clause, parameters)
+    # Commit the changes to the database
     conn.commit()
     conn.close()
 
@@ -45,10 +69,47 @@ def insertWeather(parameters):
 def insertPhoto(parameters):
     conn = sqlite3.connect(path)
     sql = conn.cursor()
+    # Clean up Weather table from old data, here we need to trim LoggedTime to Y-m-s format and delete everything that is not 
+    # from today.
+    clause = "delete from Photo where date(LoggedTime) < date()"
+    sql.execute(clause)
+    # Insert the data from parameters into the Photo table
     clause = "insert into Photo "
     clause += "(LoggedTime, MeasuredTime, B64Photo) "
-    clause += "values (?, ? , ?)"
+    clause += "values (?, ?, ?)"
     sql.execute(clause, parameters)
+    # Commit the changes to the database
+    conn.commit()
+    conn.close()
+
+# Insert date of data gotten from the electricity api call to the table ElectricityLog
+def insertElectricityLog(parameters):
+    conn = sqlite3.connect(path)
+    sql = conn.cursor()
+    # Clean up ElectricityLog table of dates older than a week
+    clause = "delete from ElectricityLog where DataFetched < date(date(), '-7 days')"
+    sql.execute(clause)
+    # Insert the data from parameters into the ElectricityLog table
+    clause = "insert into ElectricityLog (DataFetched) values (?)"
+    sql.execute(clause, parameters)
+    # Commit the changes to the database
+    conn.commit()
+    conn.close()
+
+# Insert data from the electricity api call to the table ElectricityPrices
+def insertElectricityPrices(parameters):
+    conn = sqlite3.connect(path)
+    sql = conn.cursor()
+    # Clean up ElectricityPrices table
+    clause = "delete from ElectricityPrices where PriceDate < date(date(), '-7 days')"
+    sql.execute(clause)
+    # Insert the data from parameters to the ElectricityPrices table
+    clause = "insert into ElectricityPrices "
+    clause += "(PriceDate, TimeID, DateHour, Price) "
+    clause += "values (?, ?, ?, ?)"
+    # This time parameters is a list of tuples, so we need to use the executemany function
+    sql.executemany(clause, parameters)
+    # Commit the changes to the database
     conn.commit()
     conn.close()
 
@@ -56,9 +117,11 @@ def insertPhoto(parameters):
 def getLatestWeather():
     conn = sqlite3.connect(path)
     sql = conn.cursor()
+    # Get the latest data from the Weather table
     clause = "select LoggedTime, MeasuredTime, Temperature, WindSpeed, WindDirection, Moisture, Rain, RainDescFi, RainDescEn "
     clause += "from Weather "
     clause += "where LoggedTime = (select max(LoggedTime) from Weather)"
+    # Get the one result row that this select grabs from database and return it
     value = sql.execute(clause).fetchone()
     conn.close()
     return value
@@ -67,8 +130,23 @@ def getLatestWeather():
 def getLatestPhoto():
     conn = sqlite3.connect(path)
     sql = conn.cursor()
+    # Get the latest data from the Photo table
     clause = "select LoggedTime, MeasuredTime, B64Photo from Photo where LoggedTime = (select max(LoggedTime) from Photo)"
+    # Get the one result row that this select grabs from database and return it
     value = sql.execute(clause).fetchone()
     conn.close()
     return value
 
+# Check if given date can be found in ElectricityLog table
+def checkElectricityDate(parameters):
+    conn = sqlite3.connect(path)
+    sql = conn.cursor()
+    # Check if date is in database
+    clause = "select count(DataFetched) from ElectricityLog where DataFetched = ?"
+    value = sql.execute(clause, parameters).fetchone()
+    conn.close()
+    if value[0] == 0:
+        return False
+    else:
+        return True
+    
--- config-site/weather_api.py
+++ config-site/weather_api.py
@@ -1,9 +1,26 @@
 # Import libraries
 from datetime import datetime
 from requests import get
-import sql
+from sql import createTables, getLatestPhoto, getLatestWeather, insertPhoto, insertWeather
 import photo
 import config
+
+def windSpeed(speed):
+    if 0 <= speed < 1:
+        return "Tyyntä"
+    if 1 <= speed < 4:
+        return "Heikkoa"
+    if 4 <= speed < 8:
+        return "Kohtalaista"
+    if 8 <= speed < 14:
+        return "Navakkaa"
+    if 14 <= speed < 21:
+        return "Kovaa"
+    if 21 <= speed < 33:
+        return "Myrsky"
+    if 33 <= speed:
+        return "Hirmumyrsky"
+    
 
 def getWeatherData():
     return_value = dict()
@@ -11,10 +28,11 @@ def getWeatherData():
 
     # Digitraffic.fi allows maximum of 60 apicalls per minute, so we are going to store the results into the database and only get
     # refreshed periodically in order to not get blocked by digitraffic. 
-    sql.createTables()
-    weather = sql.getLatestWeather()
-    photoinfo = sql.getLatestPhoto()
+    createTables()
+    weather = getLatestWeather()
+    photoinfo = getLatestPhoto()    
 
+    # Get the current datetime value
     current_time = datetime.now()
 
     # If database is empty
@@ -37,7 +55,9 @@ def getWeatherData():
 
     # Do the api call    
     if getapi == True:
+        # print this to terminal running the framework to see where the data came from
         print("api")
+
         # Url for open source api call, provider digitraffic.fi, which maintains lots of weather stations along Finnish road network
         # This site is located alongside Highway no 4, Oulu (Intiö), Finland (https://tie.digitraffic.fi/api/weather/v1/stations/12019)
         # List of all weather stations: https://tie.digitraffic.fi/api/weather/v1/stations
@@ -61,7 +81,7 @@ def getWeatherData():
                     sensors[16]['value'], sensors[16]['sensorValueDescriptionFi'],  sensors[16]['sensorValueDescriptionEn'])
         
         # Insert values to Weather table
-        sql.insertWeather(parameters)
+        insertWeather(parameters)
         
         # Get the photo metainfo
         url = "https://tie.digitraffic.fi/api/weathercam/v1/stations/C12612/data"
@@ -82,7 +102,13 @@ def getWeatherData():
         # Create tuple of parameters to be inserted to Photo table
         parameters = (current_time, localpresettime, b64photo)
         # Insert values to Photo table
-        sql.insertPhoto(parameters)
+        insertPhoto(parameters)
+
+        # Draw wind direction compass and save it as compass.jpg
+        photo.drawArrow(sensors[14]['value'])
+
+        # Get wind speed description
+        windspeed = windSpeed(sensors[12]['value'])
 
         # SensorValueDescriptionFi values
         # - Pouta
@@ -102,6 +128,7 @@ def getWeatherData():
                         sensors[16]['name']:sensors[16]['value'],
                         sensors[16]['name']+"_KUVAUS":sensors[16]['sensorValueDescriptionFi'],
                         sensors[16]['name']+"_DESC":sensors[16]['sensorValueDescriptionEn'],
+                        "TUULISELITE": windspeed,
                         "KUVAUSAIKA":localpresettime,
                         "KUVA": b64photo}
         
@@ -125,7 +152,12 @@ def getWeatherData():
 
     # Return values from sql database
     else:
+        # print this to terminal running the framework to see where the data came from
         print("sql")
+
+        # Get wind speed description
+        windspeed = windSpeed(weather[3])
+
         # Create dictionary from sql values
         return_value = {"MITTAUSAIKA":weather[1],
                         "ILMA":weather[2],
@@ -135,6 +167,7 @@ def getWeatherData():
                         "SADE":weather[6],
                         "SADE_KUVAUS":weather[7],
                         "SADE_DESC":weather[8],
+                        "TUULISELITE":windspeed,
                         "KUVAUSAIKA": photoinfo[1],
                         "KUVA": photoinfo[2]}
     # Return the correct dictionary
