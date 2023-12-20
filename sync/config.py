354593607bb0795c04fce6ec8c0106534f58f64e
Zame76 <148104936+Zame76@users.noreply.github.com>
2023-12-20 11:39:10 +0200

    Delete sync/config.py

--- sync/config.py
+++
@@ -1,6 +0,0 @@
-# Insert security key for the api calls
-seckey = "234wjke2-klk32mbu2-r233o-salp21m-aonv235ms1-kaoie3-apwemnw2345mslk1"
-# Set url for the api interface
-url = "http://localhost:5000/dataexchange"
-# Set path for database
-path = "C:/Koodit/Python/projektiomega/db/"
